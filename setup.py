8b6586958233f797591ffce4a810bcb13ea27829
简律纯 <i@jyunko.cn>
2023-07-04 12:23:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-from setuptools import setup, find_packages
+[build-system]
+requires = ["setuptools>=42", "wheel"]
+build-backend = "setuptools.build_meta"
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+[tool.poetry]
+name = "HydroRollCore"
+version = "0.1.1"
+authors = ["简律纯 <i@jyunko.cn>"]
+description = "Core of HydroRoll."
+long_description = """
+The long description of your project.
+"""
+keywords = ["HydroRoll", "core"]
+homepage = "https://github.com/HydoRoll-Team/HydroRollCore"
 
-setup(
-    name='HydroRollCore',
-    version='0.1.1',
-    author='简律纯',
-    author_email='i@jyunko.cn',
-    description='Core of HydroRoll.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/HydoRoll-Team/HydroRollCore',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    packages=find_packages(),
-    python_requires='>=3.8',
-    install_requires=['tkinter'],
-    entry_points={
-        'console_scripts': [
-            'HydroRoll = HydroRoll.__init__:main'
-        ]
-    },
-)
+[tool.poetry.dependencies]
+python = ">=3.8"
+tkinter = "^8.6.10"
+
+[build-system]
+requires = ["poetry-core>=1.0.0"]
+build-backend = "poetry.core.masonry.api"
+
+[tool.poetry.plugins."poetry-plugin"]
+class = "poetry_plugin.plugin.Plugin"
+
+[tool.poetry.plugins."poetry-plugin".scripts]
+HydroRoll = "HydroRoll.__init__:main"
