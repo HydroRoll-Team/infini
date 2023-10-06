fd1f123d531e25ac066dae6a1ea8dc19fd1c0964
简律纯 <i@jyunko.cn>
2023-10-07 02:50:20 +0800

    feat: BREAKING CHANGES
    Co-authored-by: 白咕咕 <baimianxiao@users.noreply.github.com>
    Co-authored-by: kenichiLyon <kenichiLyon@users.noreply.github.com>

--- HydroRollCore/__init__.py
+++
@@ -1,7 +0,0 @@
-name = "HydroRollCore"
-
-from HydroRollCore.core import Core
-from HydroRollCore.rule import Rule, RuleLoadType
-from HydroRollCore.config import ConfigModel
-
-__all__ = ['Core', 'Rule', 'ConfigModel', 'RuleLoadType']
---
+++ src/hydrorollcore/__init__.py
@@ -0,0 +1,7 @@
+name = "HydroRollCore"
+
+from HydroRollCore.core import Core
+from HydroRollCore.rule import Rule, RuleLoadType
+from HydroRollCore.config import ConfigModel
+
+__all__ = ['Core', 'Rule', 'ConfigModel', 'RuleLoadType']
