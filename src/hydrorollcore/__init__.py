d62900046bb6f754a8e6e7e670a66a90134055d9
简律纯 <i@jyunko.cn>
2023-10-07 03:45:44 +0800

    chore: sync version

--- src/hydrorollcore/__init__.py
+++ src/hydrorollcore/__init__.py
@@ -2,6 +2,5 @@ from HydroRollCore.cli import Cli
 from HydroRollCore.config import ConfigModel
 from HydroRollCore.rule import Rule, RuleLoadType
 from HydroRollCore.core import Core
-__version__ = "0.1.1"
 
 __all__ = ['Core', 'Rule', 'ConfigModel', 'RuleLoadType', 'Cli']
