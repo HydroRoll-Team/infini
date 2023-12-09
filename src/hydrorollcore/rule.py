e6b74981c6f713fb30d9c439cce2c1226d36acb5
苏向夜 <fu050409@163.com>
2023-12-09 21:20:40 +0800

    :sparkles: 新增掷骰基类

--- src/hydrorollcore/rule.py
+++ src/hydrorollcore/rule.py
@@ -13,6 +13,34 @@ class RuleLoadType(Enum):
     CLASS = "class"
 
 
+class Result(metaclass=ABCMeta):
+    """规则检定结果基类"""
+
+    event: str
+
+
+class Dice(metaclass=ABCMeta):
+    """掷骰基类"""
+
+    roll_string: str
+    db: str
+    outcome: int
+
+    def __repr__(self) -> str:
+        return f'<HydroDice "{self.db.upper()}">'
+
+    def __str__(self) -> str:
+        return self.db.upper()
+
+    @abstractmethod
+    def parse(self) -> "Dice":
+        raise NotImplementedError
+
+    @abstractmethod
+    def roll(self) -> int:
+        raise NotImplementedError
+
+
 class Rule(metaclass=ABCMeta):
     """规则基类"""
 
@@ -20,9 +48,9 @@ class Rule(metaclass=ABCMeta):
     priority: int = 0
 
     @abstractmethod
-    def __init__(self):
+    def __init__(self) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    async def run(self):
+    def check(self) -> Result:
         raise NotImplementedError
