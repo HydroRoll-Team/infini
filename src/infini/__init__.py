6e5183486c1ad7a8bac128851dff7758cb1c3cd2
苏向夜 <fu050409@163.com>
2023-12-15 11:08:08 +0800

    :sparkles: 新增__init__.py入口模块

--- src/infini/__init__.py
+++ src/infini/__init__.py
@@ -1,4 +1,5 @@
 from infini.handler import Handler, Result
 from infini.event import MessageEvent
+from infini.matcher import matcher
 
-__all__ = ["Handler", "Result", "MessageEvent"]
+__all__ = ["Handler", "Result", "MessageEvent", "matcher"]
