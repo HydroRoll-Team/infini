68ab4a7d835e7ede363d3bc9fa2731a4a335f4a0
简律纯 <i@jyunko.cn>
2023-12-18 02:27:15 +0800

    refactor(exceptions): 重定义三个异常基类

--- src/infini/exceptions.py
+++ src/infini/exceptions.py
@@ -1,7 +1,7 @@
 """Infini 异常处理模块
 
 此模块定义了 Infini 项目中所有的自定义异常类。
-规则包作者后续实现的每个异常类都应该继承自 InfiniBaseError。
+规则包作者后续实现的每个异常类都应该继承自 RulePackageException。
 """
 
 
@@ -20,15 +20,17 @@ class StopException(EventException):
 class InfiniException(Exception):
     """Infini 异常基类"""
 
+
 class RulePackageException(InfiniException):
     """由规则包抛出的异常基类, 所有规则包抛出的异常都应该继承此类。"""
 
-class LoadError(HydroError):
-    """规则包导入错误"""
+
+class LoadError(InfiniException):
+    """加载规则包错误, 找不到指定的规则包或事件声明有误导致运行时错误时抛出。"""
 
 
 class PackageNotFound(LoadError):
-    """规则包不存在"""
+    """规则包不存在时错误, """
 
 
 class EventLoadError(LoadError, RuntimeError):
@@ -39,21 +41,17 @@ class HandlerLoadError(LoadError, RuntimeError):
     """业务函数导入失败"""
 
 
-class EventError(HydroError):
-    """事件处理时异常"""
+class UnknownException(BaseException):
+    """未知异常基类"""
 
 
-class UnknownEvent(EventError):
-    """未知事件"""
-
-
-class UnknownMatcherEvent(UnknownEvent):
+class UnknownMatcherEvent(UnknownException):
     """未知的给入实现"""
 
 
-class UnknownMessageEvent(UnknownEvent):
+class UnknownMessageEvent(UnknownException):
     """未知的给出实现"""
 
 
-class UnsupportedError(EventError, RuntimeError):
+class UnsupportedError(EventException, RuntimeError):
     """方法未被支持"""
