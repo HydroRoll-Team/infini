037a5b0e2c1ec9ffa8f0837d019ec8d6ebd20a36
苏向夜 <fu050409@163.com>
2023-12-17 23:37:35 +0800

    :fire: 清理废弃代码

--- src/infini/handler.py
+++ src/infini/handler.py
@@ -4,27 +4,11 @@ from .event import MatcherEvent, InfiniEvent
 __all__ = ["Handler"]
 
 
-# class Result:
-#     """规则包运行结果"""
-
-#     event: str
-#     status: bool
-#     kwargs: dict = {}
-
-#     def __init__(self, event: str, status: bool, **kwargs) -> None:
-#         self.event = event
-#         self.status = status
-#         self.kwargs = kwargs
-
-
 class Handler(metaclass=ABCMeta):
     """规则包业务基类"""
 
     name: str
     priority: int = 0
-
-    # def __init_subclass__(cls) -> None:
-    #     handlers.regist(cls.name, cls())
 
     @abstractmethod
     def process(self, event: MatcherEvent) -> InfiniEvent:
