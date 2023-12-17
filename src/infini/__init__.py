632b301a0474c7cb2816a01592cab166f83ace0a
简律纯 <i@jyunko.cn>
2023-12-18 01:54:16 +0800

    chore(exp): add explaination at `__init__.py`

--- src/infini/__init__.py
+++ src/infini/__init__.py
@@ -1,3 +1,18 @@
+"""Infini
+
+Core of HydroRoll, python version of GRPS-1.
+
+
+本模块从子模块导入了以下内容：
+- `Handler` => [`infini.handler`](./handler#Handler)
+- `MessageEvent` => [`infini.event.MessageEvent`](./event#MessageEvent)
+- `MatcherEvent` => [`infini.event.MatcherEvent`](./event#MatcherEvent)
+- `WorkflowEvent` => [`infini.event.WorkflowEvent`](./event#WorkflowEvent)
+- `Matcher` => [`infini.matcher.Matcher`](./matcher/#Matcher)
+- `Register` => [`infini.register.Register`](./register#Register)
+"""
+
+
 from infini.handler import Handler
 from infini.event import MessageEvent, MatcherEvent, WorkflowEvent
 from infini.matcher import Matcher
