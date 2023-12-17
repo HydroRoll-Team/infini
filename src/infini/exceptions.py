7b219aa1294905fb1955dba15ae7c9353d24cf0e
简律纯 <i@jyunko.cn>
2023-12-18 02:16:07 +0800

    feat(event): add `EventException` `SkipException` `StopException`

--- src/infini/exceptions.py
+++ src/infini/exceptions.py
@@ -1,3 +1,22 @@
+"""Infini 异常处理模块
+
+此模块定义了 Infini 项目中所有的自定义异常类。
+规则包作者后续实现的每个异常类都应该继承自 InfiniBaseError。
+"""
+
+
+class EventException(BaseException):
+    """事件处理过程中由规则包抛出的异常, 用于控制事件的传播, 会被 Infini 自动捕获并处理。"""
+
+
+class SkipException(EventException):
+    """跳过当前规则包继续当前事件传播。"""
+
+
+class StopException(EventException):
+    """停止当前事件传播。"""
+
+
 class HydroError(Exception):
     """HydroRoll 异常基类"""
 
