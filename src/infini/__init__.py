8850fb24d8e8a9c601f468dda256dec22b7d014b
苏向夜 <fu050409@163.com>
2023-12-18 01:14:38 +0800

    :recycle: 修订入口文件

--- src/infini/__init__.py
+++ src/infini/__init__.py
@@ -1,5 +1,13 @@
 from infini.handler import Handler
-from infini.event import MessageEvent
-from infini.matcher import matcher
+from infini.event import MessageEvent, MatcherEvent, WorkflowEvent
+from infini.matcher import Matcher
+from infini.register import Register
 
-__all__ = ["Handler", "MessageEvent", "matcher"]
+__all__ = [
+    "Handler",
+    "MessageEvent",
+    "Matcher",
+    "Register",
+    "MatcherEvent",
+    "WorkflowEvent",
+]
