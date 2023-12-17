31b0e745ab9688c3c760130ded3e0ea645902b15
苏向夜 <fu050409@163.com>
2023-12-18 01:13:19 +0800

    :bug: 允许Handler实例化

--- src/infini/handler.py
+++ src/infini/handler.py
@@ -4,11 +4,14 @@ from .event import MatcherEvent, InfiniEvent
 __all__ = ["Handler"]
 
 
-class Handler(metaclass=ABCMeta):
+class Handler:
     """规则包业务基类"""
 
     name: str
     priority: int = 0
+
+    def __init__(self) -> None:
+        pass
 
     @abstractmethod
     def process(self, event: MatcherEvent) -> InfiniEvent:
--- src/infini/matcher.py
+++ src/infini/matcher.py
@@ -1,4 +1,4 @@
-from .register import Events, Handlers, events, handlers
+from .register import Register, register
 from .event import MatcherEvent
 from .handler import Handler
 from .exceptions import UnknownMatcherEvent
@@ -7,24 +7,23 @@ from .exceptions import UnknownMatcherEvent
 class Matcher:
     """事件处理单元"""
 
-    events: Events
-    handlers: Handlers
+    register: Register
 
-    def __init__(
-        self, _events: Events | None = None, _handlers: Handlers | None = None
-    ) -> None:
-        self.events = _events if _events else events
-        self.handlers = _handlers if _handlers else handlers
+    def __init__(self, _register: Register | None = None) -> None:
+        self.register = _register if _register else register
 
     def match(self, name: str) -> Handler:
-        if handler := self.handlers.match(name):
-            return handler
+        if handler := self.register.handlers.match(name):
+            if isinstance(handler, Handler):
+                return handler
+            else:
+                return handler()
         else:
             raise UnknownMatcherEvent(f"未知的规则包: {name}")
 
     def run(self, event: MatcherEvent) -> str:
         callback_event = self.match(event.name).process(event)
-        return self.events.process(
+        return self.register.events.process(
             callback_event.name,
             **callback_event.kwargs if callback_event.kwargs else callback_event.kwargs,
         )
