e999efafbc75133b03211dbecaf9bc424bf478ff
苏向夜 <fu050409@163.com>
2024-01-26 23:28:29 +0800

    :sparkles: feat(ci): add new generator tests

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -9,3 +9,30 @@ def test_generator():
     }
     generator.match(Output("text", "test.event1"))
     assert generator.output(Output("text", "test.event1")) == "Event1 文本"
+
+
+def test_generator_with_var():
+    generator = Generator()
+    generator.events = {
+        "test.event1": "Event1 文本: {{ var }}",
+    }
+
+    assert (
+        generator.output(Output("text", "test.event1", variables={"var": "变量测试"}))
+        == "Event1 文本: 变量测试"
+    )
+
+
+def test_generator_with_function():
+    def add(a, b):
+        return a + b
+
+    generator = Generator()
+    generator.events = {
+        "test.event": "{{ func(1, 2) }}",
+    }
+
+    assert (
+        generator.output(Output("text", "test.event", variables={"func": add}))
+        == "3"
+    )
